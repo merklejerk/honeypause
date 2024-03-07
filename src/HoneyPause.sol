9516fe75e1621e6036305c6493e62638e5f4d6bc
Lawrence Forman <me@merklejerk.com>
2024-03-07 09:46:30 -0500

    Update src/HoneyPause.sol
    Co-authored-by: Justin Schuldt <justinschuldt@gmail.com>

--- src/HoneyPause.sol
+++ src/HoneyPause.sol
@@ -107,6 +107,7 @@ contract HoneyPause {
     /// @param payoutAmount The bounty amount (in wei).
     /// @param verifier The protocol's verifier contract, which asserts that critical invariants have been broken.
     /// @param pauser A privileged contract that can pause the protocol when called.
+    /// @param payer A contract that can pay the bounty when called.
     /// @param operator Who can cancel this bounty.
     /// @return bountyId The ID of the created bounty.
     function add(
